e725a952f6e349adf1b82175079746e56840e8eb
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-10-10 17:00:53 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ const squareRoot = (num: number): number => {
     const numberifed = numberify(num)
     return Nath.sqrt(numberifed)
 }
+const exponential = (num: number, num2: number): number => {
+    const numberifed = numberify(num)
+    const numberifed2 = numberify(num2)
+    return Math.pow(numberifed1, numberifed2)
+}
 
 
 
-export { square, squareRoot }
+export { square, squareRoot, exponential }
